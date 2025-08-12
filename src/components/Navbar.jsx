1d73ec6d40c911d29d7c78d935a4ab106b8f06c5
minhajsvn <minhajrafeeq7@gmail.com>
2025-08-12 20:04:13 +0530

    Updated Logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Logo from '../assets/logo.svg'
+import Logo from '../assets/Logo.svg'
 
 export default function Navbar() {
     const [isMenuOpen, setIsMenuOpen] = useState(false)
