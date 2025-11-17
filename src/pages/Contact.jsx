47ec1f0c61fd855c2f51d7a54f483f1732046d9e
Mohammad Minhaj <minhajrafeeq7@gmail.com>
2025-11-17 15:13:44 +0530

    Updated the contact details in contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -345,11 +345,23 @@ const Contact = () => {
                 </h2>
                 <div className="space-y-4">
                   <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between text-base text-gray-600">
-                    <div className="flex items-center mb-2 sm:mb-0">
-                      <svg className="w-5 h-5 mr-2 text-[#DCBC7C]" fill="currentColor" viewBox="0 0 20 20">
-                        <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"/>
-                      </svg>
-                      <span>Tel: +971 54 265 0191</span>
+                    <div className="flex flex-col sm:flex-col gap-0">
+                      <div
+                        onClick={() => window.location.href = 'tel:+971525647522'}
+                        className="flex items-center mb-2 cursor-pointer"
+                      >
+                        <svg className="w-5 h-5 mr-2 text-[#DCBC7C]" fill="currentColor" viewBox="0 0 20 20">
+                          <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"/>
+                        </svg>
+                        <span>Tel: +971 52 564 7522</span>
+                      </div>
+                      <div
+                        onClick={() => window.location.href = 'tel:+971565956353'}
+                        className="flex items-center ml-14 cursor-pointer"
+                      >
+                        
+                        <span>+971 56 595 6353</span>
+                      </div>
                     </div>
                     <div className="flex items-center">
                       <svg className="w-5 h-5 mr-2 text-[#DCBC7C]" fill="currentColor" viewBox="0 0 20 20">
